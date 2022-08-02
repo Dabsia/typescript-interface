0f3ef3045c35ab9d18ecfd94b9a20baf3f58d72b
Dabo Johnson <dabojohnson98@gmail.com>
2022-08-01 23:34:45 -0700

    Learnt type casting

--- interface.ts
+++ interface.ts
@@ -17,3 +17,29 @@ user1 = {
 
 user1.greet('Hello, Good evening')
 
+interface Bird {
+    flyingSpeed: number;
+    type: 'bird'
+}
+
+interface Horse {
+    runningSpeed: number;
+    type: 'horse'
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal): void {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed
+    }
+
+    console.log('The animal is moving at', speed)
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 10})
